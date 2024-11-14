22faaeb701a9409565825c9143752310c1f52b7e
Marco Ocanto <mocanto@cnti.gob.ve>
2024-11-14 18:06:17 -0400

    cambios index

--- index.js
+++ index.js
@@ -2,3 +2,49 @@ const express = require('express')// Express sera el metodoto para construir la 
 const app = express ()
 const port= 3000
 
+app.get('/',(req,res)=> {
+res.send('Server en Express')
+})
+app.get('/segunda-ruta',(req,res)=> {
+  res.send('Segunda ruta')
+})
+app.get('/tercera-ruta',(req,res)=> {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+
+  })
+    }
+  )
+  app.get('/cuarta-ruta',(req,res)=> {
+    res.json([
+      {
+      name: 'Categoria 1',
+      tipo: 'Admin'
+
+    },
+    {
+      name: 'Categoria 2',
+      tipo: 'Secretario'
+
+    }
+    ])
+  })
+  app.get('/cuarta-ruta/:id',(req,res)=> {
+    const {id}= req.params
+    res.json(
+
+      {
+        name: 'Categoria 2',
+        tipo: 'Secretario'
+
+      }
+    )
+  })
+
+
+app.listen(port,()=>{
+  console.log('Puerto' + port)
+
+})
+
